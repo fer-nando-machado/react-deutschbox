831b3ef275a9e54294e04952668f09345a0e76f3
Fernando Machado <lfmachado90@gmail.com>
2024-07-05 15:30:12 +0200

    converts css to scss

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,8 @@ import DeutschBox from "./DeutschBox";
 import GitHubLogo from "./assets/github.svg";
 import NPMLogo from "./assets/npm.svg";
 
-import "./App.css";
+import "./App.scss";
+import BubbleLabel from "./BubbleLabel";
 
 function App() {
   const [checked, setChecked] = useState(false);
@@ -21,20 +22,21 @@ function App() {
         checked={checked}
         onChange={setChecked}
       />
+      <p style={{ display: "none" }}>
+        <BubbleLabel direction="left">hello</BubbleLabel>
+        <BubbleLabel direction="right">turn</BubbleLabel>
+        <BubbleLabel direction="top">your</BubbleLabel>
+        <BubbleLabel direction="bottom">radio</BubbleLabel>
+        <BubbleLabel>on</BubbleLabel>
+      </p>
       <h4>
         <code>checked: {checked.toString()}</code>
       </h4>
       <div className="external">
-        <a
-          href="https://github.com/fer-nando-machado/react-deutschbox"
-          target="_blank"
-        >
+        <a href="https://github.com/fer-nando-machado/react-deutschbox">
           <img src={GitHubLogo} alt="react-deutschbox @ GitHub" height="25" />
         </a>
-        <a
-          href="https://www.npmjs.com/package/react-deutschbox"
-          target="_blank"
-        >
+        <a href="https://www.npmjs.com/package/react-deutschbox">
           <img src={NPMLogo} alt="react-deutschbox @ npm" height="18" />
         </a>
       </div>
