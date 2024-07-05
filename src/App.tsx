b39f1c1e67a6d242c540e85cfb6b0838b2e067cb
Fernando Machado <lfmachado90@gmail.com>
2024-07-05 21:24:25 +0200

    adds usage examples

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,80 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import DeutschBox from "./DeutschBox";
 import GitHubLogo from "./assets/github.svg";
 import NPMLogo from "./assets/npm.svg";
 
 import "./App.scss";
-import BubbleLabel from "./BubbleLabel";
 
 function App() {
+  const ref = useRef<HTMLInputElement>(null);
+
   const [checked, setChecked] = useState(false);
+  const onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setChecked(event.target.checked);
+  };
 
   return (
     <div className="App">
       <h1>
         <code>{"<DeutschBox/>"}</code>
       </h1>
-      <DeutschBox
-        name="demo"
-        feedback
-        color="#DD0000"
-        size="75px"
-        checked={checked}
-        onChange={setChecked}
-      />
-      <p style={{ display: "none" }}>
-        <BubbleLabel direction="left">hello</BubbleLabel>
-        <BubbleLabel direction="right">turn</BubbleLabel>
-        <BubbleLabel direction="top">your</BubbleLabel>
-        <BubbleLabel direction="bottom">radio</BubbleLabel>
-        <BubbleLabel>on</BubbleLabel>
-      </p>
-      <h4>
-        <code>checked: {checked.toString()}</code>
-      </h4>
+
+      <h3>usage examples</h3>
+
+      <div className="example">
+        <div className="description">
+          <h4>with state</h4>
+          <code>
+            checked: <b>{checked.toString()}</b>
+          </code>
+        </div>
+        <DeutschBox checked={checked} onChange={onChange} size={75} feedback />
+      </div>
+      <textarea spellCheck={false}>
+        {`const [checked, setChecked] = useState(false);
+const onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+  setChecked(event.target.checked);
+};
+
+<DeutschBox
+  checked={checked}
+  onChange={onChange}
+  size={75}
+  feedback
+/>`}
+      </textarea>
+
+      <div className="example">
+        <div className="description">
+          <h4>with references</h4>
+          <button onClick={() => alert(ref.current?.checked)}>
+            <code>alert(ref.current?.checked)</code>
+          </button>
+        </div>
+        <DeutschBox
+          id="demo-id"
+          name="demo-name"
+          ref={ref}
+          checked={true}
+          color="#DD0000"
+          size={75}
+          readOnly
+        />
+      </div>
+      <textarea spellCheck={false}>
+        {`const ref = useRef<HTMLInputElement>(null);
+
+<DeutschBox
+  id="demo-id"
+  name="demo-name"
+  ref={ref}
+  checked={true}
+  color="#DD0000"
+  size={75}
+  readOnly
+/>`}
+      </textarea>
+
       <div className="external">
         <a href="https://github.com/fer-nando-machado/react-deutschbox">
           <img src={GitHubLogo} alt="react-deutschbox @ GitHub" height="25" />
